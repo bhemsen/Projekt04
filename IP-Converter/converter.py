763eaf358e28448fbd11b420897f11c76710d84b
bhemsen <bhemsen@medienwerft.de>
2021-02-08 17:43:53 +0100

    update Inputhandler

--- IP-Converter/converter.py
+++ IP-Converter/converter.py
@@ -3,9 +3,9 @@ from ip4to6 import *
 from ip6to4 import *
 from inputhandler.inputhandler import Inputhandler
 
-Inputhandler.handleIPv4(Inputhandler)
+inputV4 = Inputhandler.handleIPv4(Inputhandler)
 
-converter = IPv4ToIPv6('192.168.0.255')
+converter = IPv4ToIPv6(inputV4)
 converter.convert()
 
 converter2 = IPv6ToIPv4(converter.convert())
