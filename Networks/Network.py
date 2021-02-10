4e6962958d46ef9ccb157c183291b702c76e572d
bhemsen <bhemsen@medienwerft.de>
2021-02-10 21:44:52 +0100

    add funktions to create and edit subnets

---
+++ Networks/Network.py
@@ -0,0 +1,19 @@
+from netaddr import * 
+from inputhandler.inputhandler import Inputhandler
+from IPConverter.ConvertInputToNetwork import ConvertInputToNetwork
+ 
+class Network:
+    def __init__(self) -> None:
+        pass
+
+# take an IPNetwork object and the data dictionary the get the Suffix for subnets
+# returns a dict containig the created subnets with a list of their ip's 
+    def createSubnets(self, data, ipNetwork):
+        subnets = list(ipNetwork.subnet(data['subnet']))
+        networkList = {}
+        for subnet in subnets:
+            networkList[str(subnet)] = list(subnet)
+        return networkList
+
+
+
